266a7278e203ac83476d90d125d04693a9cd4a0f
Eric <emoolenbeek@gmail.com>
2024-11-30 21:23:32 -0500

    update navigation

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
+	import { Button } from "$lib/components/ui/button";
 </script>
 
-<nav class="navigation">
-	<h1>Website</h1>
+<nav class="navigation border-b">
+	<div class="container flex h-14 items-center px-4 max-w-7xl mx-auto">
+		<h1 class="text-lg font-semibold"><a href="/">Website</a></h1>
+		
+		<div class="flex items-center gap-4 ml-auto">
+			<Button variant="ghost" href="/about">About</Button>
+			<Button variant="ghost" href="/contact">Contact</Button>
+		</div>
+	</div>
 </nav>
