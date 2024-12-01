c2d92c86d5352bd9d3d522def39db182d9e8c8d3
Eric <emoolenbeek@gmail.com>
2024-11-30 21:23:52 -0500

    create layout and import nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+    import type { LayoutData } from './$types';
+    import '../app.pcss';
+    import Navigation from '$lib/components/Navigation.svelte';
+
+    let { data, children }: { data: LayoutData, children: Snippet } = $props();
+</script>
+
+<Navigation />
+{@render children()}
